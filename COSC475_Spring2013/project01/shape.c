82e6d1803f6b085eba1f25ae4a1d3de5301a21a2
cswells <cswells87@gmail.com>
2013-03-05 23:35:14 -0500

    Update

--- COSC475_Spring2013/project01/shape.c
+++ COSC475_Spring2013/project01/shape.c
@@ -1,46 +1,46 @@
-#include <GL/glut.h>
+//#include <GL/glut.h>
 
-void myinit()
-{
-	glClearColor(1.0, 1.0, 1.0, 1.0); //white background
-	glColor3f(1.0, 0.0, 0.0); //Draw in Red
+//void myinit()
+//{
+//	glClearColor(1.0, 1.0, 1.0, 1.0); //white background
+//	glColor3f(1.0, 0.0, 0.0); //Draw in Red
+//
+//	glMatrixMode(GL_PROJECTION);
+//	glLoadIdentity();
+//	gluOrtho2D(0.0, 500.0, 0.0, 500.0);
+//	glMatrixMode(GL_MODELVIEW);
+//
+//}
 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0.0, 500.0, 0.0, 500.0);
-	glMatrixMode(GL_MODELVIEW);
+//void display()
+//{
+//	glClear(GL_COLOR_BUFFER_BIT);
+//
+//
+//	glBegin(GL_TRIANGLE_FAN);
+//		glVertex2f(150.0,150.0); //center
+//		glVertex2f(200.0,150.0);
+//		glVertex2f(150.0,200.0);
+//		glVertex2f(100.0,150.0);
+//		glVertex2f(150.0,100.0);
+//		glVertex2f(200.0,150.0);
+//	glEnd();
+//
+//	glFlush();
+//
+//}
 
-}
-
-void display()
-{
-	glClear(GL_COLOR_BUFFER_BIT);
-
-
-	glBegin(GL_TRIANGLE_FAN);
-		glVertex2f(150.0,150.0); //center
-		glVertex2f(200.0,150.0);
-		glVertex2f(150.0,200.0);
-		glVertex2f(100.0,150.0);
-		glVertex2f(150.0,100.0);
-		glVertex2f(200.0,150.0);
-	glEnd();
-
-	glFlush();
-
-}
-
-void main(int argc, char** argv)
-{
-	glutInit(&argc,argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(500,500);
-	glutInitWindowPosition(0,0);
-	glutCreateWindow("Diamond");
-	glutDisplayFunc(display);
-
-
-	myinit();
-
-	glutMainLoop();
-}
+//void main(int argc, char** argv)
+//{
+//	glutInit(&argc,argv);
+//	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+//	glutInitWindowSize(500,500);
+//	glutInitWindowPosition(0,0);
+//	glutCreateWindow("Diamond");
+//	glutDisplayFunc(display);
+//
+//
+//	myinit();
+//
+//	glutMainLoop();
+//}
--- COSC475_Spring2013/project2/project2.c
+++ COSC475_Spring2013/project2/project2.c
@@ -1,8 +1,116 @@
-/*
- * project2.c
- *
- *  Created on: Mar 5, 2013
- *      Author: cswells
- */
+#include <GL/glut.h>
+
+void DrawMyPyramid(float x, float y, float z, float s);
+void DrawMyCube(float x, float y, float z, float s);
+static float angle = 0.00;
+void myinit()
+{
+	glClearColor(0, 0, 0, 1.0); //Black background
+	glColor3f(1.0, 0.0, 0.0); //Draw in Red
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-300.0,300.0,-300.0,300.0,300.0,-300.0);
+	glMatrixMode(GL_MODELVIEW);
+
+}
 
 
+void display()
+{
+	glLoadIdentity();
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	float x = 100.0;
+	float y = 100.0;
+	float z = 100.0;
+	float s = 100.0;
+	angle += 0.01;
+	glRotatef(angle, 0.5, 0, 0 );
+	glRotatef(angle, 0, 0.5, 0 );
+	DrawMyPyramid(x,y,z,s);
+
+	float x2 = -100.0;
+	float y2 = -100.0;
+	float z2 = -100.0;
+	float s2 = 100.0;
+	glRotatef(angle, 0, 0.5, 0 );
+	glRotatef(angle, 0.5, 0, 0 );
+	DrawMyCube(x2,y2,z2,s2);
+
+
+
+	glutPostRedisplay();
+	glFlush();
+
+}
+void DrawMyPyramid(float x, float y, float z, float s)
+{
+	glBegin(GL_TRIANGLE_FAN);
+			glColor3f(0.0, 0.0, 1.0);
+			glVertex3f(x+(s/2),	y+s,	z-(s/2));
+			glVertex3f(x, y, z);
+			glVertex3f(x+s,	y,	z);
+			glVertex3f(x+s,	y,	z-s);
+			glVertex3f(x,	y,	z-s);
+			glVertex3f(x, y, z);
+	glEnd();
+}
+
+void DrawMyCube(float x, float y, float z, float s)
+{
+	glBegin(GL_QUADS);
+		//FRONT
+		glColor3f(0.0, 0.5, 0.5);
+		glVertex3f(x, y, z);
+		glVertex3f(x, y+s, z);
+		glVertex3f(x+s,	y+s, z);
+		glVertex3f(x, y+s, z);
+		//BOTTOM
+		glColor3f(0.5, 0.0, 0.0);
+		glVertex3f(x, y, z);
+		glVertex3f(x+s, y, z);
+		glVertex3f(x+s, y, z-s);
+		glVertex3f(x, y, z-s);
+		//BACK
+		glColor3f(5.0, 0.0, 0.5);
+		glVertex3f(x, y, z-s);
+		glVertex3f(x, y+s, z-s);
+		glVertex3f(x+s,	y+s, z-s);
+		glVertex3f(x, y+s, z-s);
+		//TOP
+		glColor3f(0.0, 0.0, 0.5);
+		glVertex3f(x, y+s, z);
+		glVertex3f(x+s, y+s, z);
+		glVertex3f(x+s, y+s, z-s);
+		glVertex3f(x, y+s, z-s);
+		//LEFT
+		glColor3f(0.5, 0.5, 0.0);
+		glVertex3f(x, y, z);
+		glVertex3f(x, y+s, z);
+		glVertex3f(x, y+s, z-s);
+		glVertex3f(x, y, z-s);
+		//RIGHT
+		glColor3f(0.5, 1.0, 0.0);
+		glVertex3f(x+s, y, z);
+		glVertex3f(x+s, y+s, z);
+		glVertex3f(x+s, y+s, z-s);
+		glVertex3f(x+s, y, z-s);
+
+	glEnd();
+}
+
+void main(int argc, char** argv)
+{
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500,500);
+	glutInitWindowPosition(0,0);
+	glutCreateWindow("Diamond");
+	glutDisplayFunc(display);
+
+
+	myinit();
+
+	glutMainLoop();
+}
